16d77b11d2c39b16327a971d54aa9e66c2ae70e9
Nahuel Genchi <nagen01@gmail.com>
2022-06-07 20:34:53 -0300

    Arrow Functions

---
+++ js/13 Arrow Functions/script.js
@@ -0,0 +1,8 @@
+/*
+const addNums = (num1 = 1, num2 = 1) => {
+    return num1 + num2;
+}
+*/
+const addNums = (num1 = 1, num2 = 1) => num1 + num2; // This works like the same function above but with less code.
+
+console.log(addNums(2, 1));
